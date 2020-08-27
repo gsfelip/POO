ebb023492a11239d554123f5ee33624a98204db5
gsfelip <70300269+gsfelip@users.noreply.github.com>
2020-08-26 21:36:21 -0300

    Create Progression.java

---
+++ Atividade - Herança_Polimorfismo/Progression.java
@@ -0,0 +1,19 @@
+public class Progression{
+  protected long first;
+  protected long cur;
+
+  Progression(){cur= first= 0;}
+
+  protected long firstValue(){
+    cur = first;
+    return cur;
+  }
+  protected long nextValue(){return ++cur;}
+
+  public void printProgression(int n){
+      System.out.print(firstValue());
+      for (int i = 2; i <= n; i++)
+        System.out.print(" " + nextValue());
+      System.out.println();
+  }
+}
